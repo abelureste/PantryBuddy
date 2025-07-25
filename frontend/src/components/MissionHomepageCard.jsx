bf23240c77c83bb3f103c33c6aba9f6ab426684e
abelureste <aureste03@gmail.com>
2025-07-24 21:13:54 -0400

    Changed wording

--- frontend/src/components/MissionHomepageCard.jsx
+++ frontend/src/components/MissionHomepageCard.jsx
@@ -5,7 +5,7 @@ const MissionHomepageCard = () => {
             <div className="homepageMission">
                 <div className="missionText">
                     <h1>Our mission</h1>
-                    <p>Consumer food waste is a significant and growing problem that affects not only individual households but also the global environment and economy. In developed countries, much of the food waste occurs at the consumer level — in kitchens, restaurants, and grocery stores — where edible food is discarded due to over-purchasing, misinterpretation of expiration dates, or aesthetic preferences. This waste contributes to resource inefficiency, as the water, energy, labor, and land used to produce that food are also squandered.
+                    <p>Consumer food waste is a significant and growing problem that affects not only individual households but also the global environment and economy. In developed countries, much of the food waste occurs at the consumer level, in kitchens, restaurants, and grocery stores, where edible food is discarded due to over-purchasing, misinterpretation of expiration dates, or aesthetic preferences. This waste contributes to resource inefficiency, as the water, energy, labor, and land used to produce that food are also squandered.
                     <br/><br/>
                     Beyond the environmental toll, food waste exacerbates social issues such as food insecurity. While millions of people lack access to sufficient nutrition, vast amounts of perfectly edible food are thrown away. According to the United Nations, nearly one-third of all food produced globally is wasted, accounting for nearly 8–10% of global greenhouse gas emissions.
                     <br/><br/>
